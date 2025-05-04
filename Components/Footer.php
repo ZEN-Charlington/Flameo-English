58da9a5a7d1e21e7f8ebffb369e85c6516cbe8a7
Hieunht204 <nguyenhuynhtrunghieu1090@gmail.com>
2025-05-04 22:17:11 +0700

    Thêm footer

--- Components/Footer.php
+++ Components/Footer.php
@@ -1,1 +1,61 @@
-<nav class="Footer"> Hello this is Footer part </nav>
+<nav class="footer">
+    <div class="logo-col">
+        <img class="footer-logo" src="../assets/FlameoLogo.png">
+    </div>
+
+    <div class="footer-container">
+        
+        <div class="footer-col">
+            <p>City of Winnipeg<br>510 Main Street<br>Winnipeg, Manitoba<br>R3B 1B9</p>
+        </div>
+
+        
+        <div class="footer-col">
+            <p>Home</p>
+            <p>Accessibility</p>
+            <p>Mobile</p>
+            <p>Apps</p>
+            <p>Open Data</p>
+        </div>
+
+        <div class="footer-col">
+            <p>Residents</p>
+            <p>Business</p>
+            <p>Visitors</p>
+            <p>City Hall</p>
+            <p>Departments</p>
+        </div>
+
+        <div class="footer-col">
+            <p>Contact 311</p>
+            <p>City locations</p>
+            <p>News</p>
+            <p>Mayor’s office</p>
+            <p>Media centre</p>
+        </div>
+
+        <div class="footer-col">
+            <p>Employee portal</p>
+            <p>Careers</p>
+            <p>Site map</p>
+            <p>Privacy</p>
+        </div>
+
+        <div class="footer-col">
+            <p>Connect with us</p>
+            <div class="social-icons">
+                <span>🐦</span>
+                <span>📘</span>
+                <span>▶️</span>
+                <span>📷</span>
+                <span>💼</span>
+                <span>📡</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-note">
+        <p class="copyright">©1996–2019, City of Winnipeg</p>
+        <p>Use of this site signifies your agreement to the <a href="#">Conditions of use</a></p>
+    </div>
+</nav>
